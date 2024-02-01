dec44759c016f40781689bb9fb4baf7a213b1d7a
kamalnath dhekwar <dhekwarkamalnath18@gmail.com>
2024-02-01 22:29:34 +0530

    A .Walking Master solved

---
+++ WalkingMaster.cpp
@@ -0,0 +1,36 @@
+            // ***  ... जय श्री राम ...  ***//
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll            long long int
+#define all(x)        x.begin(),x.end()
+#define for(i,a,b)    for(ll i = a ; i<b ; i++)
+#define forr(j,c,d)   for(ll j = c ; j>=d ; j--)
+#define yes           cout<<"yes"<<endl
+#define no            cout<<"no"<<endl
+#define YES           cout<<"YES"<<endl
+#define NO            cout<<"NO"<<endl\
+#define mvi           vector<ll>
+#define mvs           vector<string>
+#define mpi           map<ll,ll>
+#define mps           map<string,string>
+#define umpi          map<string,string>
+#define umps          map<ll,ll>
+#define nn            cout<<endl
+
+int main()
+{
+    ll t;
+    cin>>t;
+    while(t--){
+        ll a,b,c,d;
+        cin>>a>>b>>c>>d;
+        if(b>d || c-a>d-b)
+        cout<<-1<<endl;
+        else cout<<a-c+2*(d-b)<<endl;
+       
+    }
+    
+    return 0;
+}
