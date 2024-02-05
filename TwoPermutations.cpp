59444ee3e12513098b2fab6d275a18921a4839c1
kamalnath dhekwar <dhekwarkamalnath18@gmail.com>
2024-02-05 22:53:57 +0530

    A . Two Permutaions solved

---
+++ TwoPermutations.cpp
@@ -0,0 +1,40 @@
+             // ***  ... जय श्री राम ...  ***//
+             
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll            long long int
+#define all(x)        x.begin(),x.end()
+#define for(i,a,b)    for(ll i = a ; i<b ; i++)
+#define forr(j,c,d)   for(ll j = c ; j>=d ; j--)
+#define yes           cout<<"yes"<<endl
+#define no            cout<<"no"<<endl
+#define YES           cout<<"YES"<<endl
+#define NO            cout<<"NO"<<endl
+#define mvi           vector<ll>
+#define mvs           vector<string>
+#define mpi           map<ll,ll>
+#define mps           map<string,string>
+#define umpi          map<string,string>
+#define umps          map<ll,ll>
+#define nn            cout<<endl
+
+int main()
+{
+    ll t;
+    cin>>t;
+    while(t--){
+        ll n,a,b;
+        cin>>n>>a>>b;
+
+        if(n==a && a==b && n==b) yes;
+        else {
+            if(a+b+1>=n) no;
+            else yes;
+        }
+      
+
+    }
+    
+    return 0;
+}
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,10 +1,1 @@
- ll count = 1;
-    //     for(i,0,n-1){
-    //      ll curr = 0;
-    //      if(v[i]==0 && v[i+1]==0){
-    //        curr++;
-    //      }
-    //      else curr = 0;
-    //      count = max(count,curr);
-            
-    //     }
+cout<<count<<" "<<key<<endl;
