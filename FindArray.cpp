db9a76536ba4007de4abd122e5d4e5273ee7d785
kamalnath dhekwar <dhekwarkamalnath18@gmail.com>
2024-02-24 17:33:37 +0530

    A Find array

---
+++ FindArray.cpp
@@ -0,0 +1,36 @@
+             // ***  ... जय श्री राम ...  ***//
+             
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll            long long int
+#define all(x)        x.begin(),x.end()
+#define for(i,a,b)    for(ll i = a ; i<b ; i++)
+#define forr(j,c,d)   for(ll j = c ; j>=d ; j--)
+#define yes           cout<<"yes"<<endl
+#define no            cout<<"no"<<endl
+#define YES           cout<<"YES"<<endl
+#define NO            cout<<"NO"<<endl
+#define mvi           vector<ll>
+#define mvc           vector<char>
+#define mvs           vector<string>
+#define mpi           map<ll,ll>
+#define mps           map<string,string>
+#define umpi          map<string,string>
+#define umps          map<ll,ll>
+#define nn            cout<<endl
+
+int main()
+{
+    ll t;
+    cin>>t;
+    while(t--){
+        ll n;
+        cin>>n;
+        for(i,2,n+2) cout<<i<<" ";
+        nn;
+        
+    }
+    
+    return 0;
+}
