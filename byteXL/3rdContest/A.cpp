a0cc3b97622206f334f26d9c8423ffa6871bcd50
kamalnath dhekwar <dhekwarkamalnath18@gmail.com>
2024-03-05 14:09:32 +0530

    byteXl

---
+++ byteXL/3rdContest/A.cpp
@@ -0,0 +1,42 @@
+             // ***  ... जय श्री राम ...  ***//
+             
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll            long long int
+#define all(x)        x.begin(),x.end()
+#define for(i,a,b)    for(ll i = a ; i<b ; i++)
+#define forr(j,c,d)   for(ll j = c ; j>=d ; j--)
+#define yes           cout<<"Yes"<<endl
+#define no            cout<<"No"<<endl
+#define YES           cout<<"YES"<<endl
+#define NO            cout<<"NO"<<endl
+#define mvi           vector<ll>
+#define mvc           vector<char>
+#define mvs           vector<string>
+#define mpi           map<ll,ll>
+#define mps           map<string,string>
+#define umpi          map<string,string>
+#define umps          map<ll,ll>
+#define nn            cout<<endl
+
+int main()
+{
+    ll n;
+    cin>>n;
+    mvi v(n);
+    string s="";
+    for(i,0,n){
+         cin>>v[i];
+         s+=to_string(v[i]);
+    }
+    string t = s;
+    reverse(all(s));
+
+    if(s==t) yes;
+    else no;
+    
+   
+    
+    return 0;
+}
