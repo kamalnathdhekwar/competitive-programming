f5ed7842bb46e359bea2b702f621250ff056bab2
kamalnath dhekwar <dhekwarkamalnath18@gmail.com>
2024-03-07 21:23:03 +0530

    byteXl

---
+++ byteXL/6thContest/M.cpp
@@ -0,0 +1,40 @@
+             // ***  ... जय श्री राम ...  ***//
+             
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll            long long int
+#define all(x)        x.begin(),x.end()
+// #define for(i,a,b)    for(ll i = a ; i<b ; i++)
+// #define forr(j,c,d)   for(ll j = c ; j>=d ; j--)
+#define yes           cout<<"Yes"<<endl
+#define no            cout<<"No"<<endl
+#define YES           cout<<"YES"<<endl
+#define NO            cout<<"NO"<<endl
+#define mvi           vector<ll>
+#define mvc           vector<char>
+#define mvs           vector<string>
+// #define mpi           map<ll,ll>
+// #define mps           map<string,string>
+// #define umpi          map<string,string>
+// #define umps          map<ll,ll>
+#define nn            cout<<endl
+
+int main()
+{
+    int t;
+    cin>>t;
+    string given = "codeforces";
+
+    while(t--)
+    {
+ 
+        string str;
+        cin>>str;
+        int n = str.length();
+        
+        string s = str;
+        string t = str;
+        
+ 
+    }
