374fab6b1c4337b723536b4a306d6a7d26560c6b
kamalnath dhekwar <dhekwarkamalnath18@gmail.com>
2024-03-10 22:09:04 +0530

    Tle Sheet 900 rated B Not Dividing solved

---
+++ TLE-900-RATED/NotDividing.cpp
@@ -0,0 +1,58 @@
+             // ***  ... जय श्री राम ...  ***//
+             
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll            long long int
+#define all(x)        x.begin(),x.end()
+#define for(i,a,b)    for(ll i = a ; i<b ; i++)
+#define forr(j,c,d)   for(ll j = c ; j>=d ; j--)
+#define yes           cout<<"Yes"<<endl
+#define no            cout<<"No"<<endl
+#define YES           cout<<"YES"<<endl
+#define NO            cout<<"NO"<<endl
+#define mvi           vector<ll>
+#define mvc           vector<char>
+#define mvs           vector<string>
+#define mpi           map<ll,ll>
+#define mps           map<string,string>
+#define umpi          map<string,string>
+#define umps          map<ll,ll>
+#define nn            cout<<endl
+
+int main()
+{
+    ll t;
+    cin>>t;
+    while(t--){
+        ll n;
+        cin>>n;
+        mvi v(n);
+        for(i,0,n) cin>>v[i];
+
+        // sort(all(v),greater<int>());
+
+        for(i,0,n-1){
+            if(v[i+1]%v[i]==0 ) {
+                 v[i]=v[i]+1;
+        
+            }
+        }
+        for(i,0,n-1){
+            if(v[i+1]%v[i]==0 ) {
+                    v[i+1]+=1;
+            }
+        }
+
+        // for(i,0,n){
+        //      if(v[i]==1) swap(v[i],v[n-1]);
+        // }
+
+        for(i,0,n) cout<<v[i]<<" ";
+
+        nn;
+        
+    }
+    
+    return 0;
+}
