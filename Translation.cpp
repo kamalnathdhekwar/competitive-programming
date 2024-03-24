f4ded7b1681de6f6ae9c5204e2a2f3dd509ca744
kamalnath dhekwar <dhekwarkamalnath18@gmail.com>
2024-03-24 21:01:24 +0530

    practice

---
+++ Translation.cpp
@@ -0,0 +1,36 @@
+             // ***  ... जय श्री राम ...  ***//
+             
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll            long long int
+#define all(x)        x.begin(),x.end()
+#define rall(x)       x.begin(),x.end()
+#define for(i,a,b)    for(ll i = a ; i<b ; i++)
+#define forr(j,c,d)   for(ll j = c ; j>=d ; j--)
+#define yes           cout<<"Yes"<<endl
+#define no            cout<<"No"<<endl
+#define YES           cout<<"YES"<<endl
+#define NO            cout<<"NO"<<endl
+#define mvi           vector<ll>
+#define mvc           vector<char>
+#define mvs           vector<string>
+#define mpi           map<ll,ll>
+#define mps           map<string,string>
+#define umpi          map<string,string>
+#define umps          map<ll,ll>
+#define nn            cout<<endl
+
+int main()
+{
+   string s,t;
+   cin>>s>>t;
+   reverse(all(s));
+
+   if(s==t) YES;
+   else NO;
+
+
+    
+    return 0;
+}
