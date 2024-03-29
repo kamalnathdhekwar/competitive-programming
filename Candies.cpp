e991efe98316d420e1f4034bd1da0be4742a9f2f
kamalnath dhekwar <dhekwarkamalnath18@gmail.com>
2024-03-29 14:28:05 +0530

    practice

---
+++ Candies.cpp
@@ -0,0 +1,54 @@
+             // ***  ... जय श्री राम ...  ***//
+             
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll            long long int
+#define all(x)        x.begin(),x.end()
+#define rall(x)       x.begin(),x.end()
+#define for(i,a,b)    for(ll i = a ; i<b ; i++)
+#define forr(j,c,d)   for(ll j = c ; j>=d ; j--)
+#define yes           cout<<"Yes"<<endl
+#define no            cout<<"No"<<endl
+#define YES           cout<<"YES"<<endl
+#define NO            cout<<"NO"<<endl
+#define mvi           vector<ll>
+#define mvc           vector<char>
+#define mvs           vector<string>
+#define mpi           map<ll,ll>
+#define mps           map<string,string>
+#define umpi          map<string,string>
+#define umps          map<ll,ll>
+#define nn            cout<<endl
+
+int main()
+{
+    ll t;
+    cin>>t;
+    while(t--){
+        ll n;
+        cin>>n;
+
+        ll sum = 1;
+        ll ans ;
+        ll i = 1;
+
+        while(true){
+
+             sum+=pow(2,i);
+             i++;
+
+            if(n%sum==0){
+                ans = n/sum;
+                break;
+            }
+            
+        }
+
+        cout<<ans<<endl;
+    }
+
+
+    
+    return 0;
+}
