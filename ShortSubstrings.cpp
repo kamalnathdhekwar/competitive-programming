6bf76773e67b0e524ace2155d23036eee18db3c1
kamalnath dhekwar <dhekwarkamalnath18@gmail.com>
2024-03-29 13:50:15 +0530

    practice

---
+++ ShortSubstrings.cpp
@@ -0,0 +1,48 @@
+             // ***  ... जय श्री राम ...  ***//
+             
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll            long long int
+#define all(x)        x.begin(),x.end()
+#define rall(x)       x.begin(),x.end()
+// #define for(i,a,b)    for(ll i = a ; i<b ; i++)
+// #define forr(j,c,d)   for(ll j = c ; j>=d ; j--)
+#define yes           cout<<"Yes"<<endl
+#define no            cout<<"No"<<endl
+#define YES           cout<<"YES"<<endl
+#define NO            cout<<"NO"<<endl
+#define mvi           vector<ll>
+#define mvc           vector<char>
+#define mvs           vector<string>
+#define mpi           map<ll,ll>
+#define mps           map<string,string>
+#define umpi          map<string,string>
+#define umps          map<ll,ll>
+#define nn            cout<<endl
+
+int main()
+{
+    ll t;
+    cin>>t;
+    while(t--){
+        ll n;
+        string s;
+        cin>>s;
+        n = s.length();
+    //     char ch1 = s[0];
+       string ans = "" ;
+      
+        for(ll i = 0 ; i<n ; i+=2){
+          ans+=s[i];
+        }
+        
+        char ch2 = s[n-1];
+        ans+=ch2;
+        cout<<ans<<endl;
+    }
+
+
+    
+    return 0;
+}
