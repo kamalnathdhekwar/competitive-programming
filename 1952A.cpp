af2a88479782ecc6178ec859f67265509d46125b
kamalnath dhekwar <dhekwarkamalnath18@gmail.com>
2024-04-01 20:40:24 +0530

    April Fools Day Contest 2024

---
+++ 1952A.cpp
@@ -0,0 +1,32 @@
+             // ***  ... जय श्री राम ...  ***//
+             
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll            long long int
+#define all(x)        x.begin(),x.end()
+#define rall(x)       x.begin(),x.end()
+#define for(i,a,b)    for(ll i = a ; i<b ; i++)
+#define forr(j,c,d)   for(ll j = c ; j>=d ; j--)
+#define yes           cout<<"Yes"<<endl
+#define no            cout<<"No"<<endl
+#define YES           cout<<"YES"<<endl
+#define NO            cout<<"NO"<<endl
+#define mvi           vector<ll>
+#define mvc           vector<char>
+#define mvs           vector<string>
+#define mpi           map<ll,ll>
+#define mps           map<string,string>
+#define umpi          map<string,string>
+#define umps          map<ll,ll>
+#define nn            cout<<endl
+
+int main()
+{
+   YES;
+
+
+
+    
+    return 0;
+}
