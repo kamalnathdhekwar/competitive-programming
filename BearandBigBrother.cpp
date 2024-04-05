5348824e5f45ef8be7c3a58f107b463995dd5bac
kamalnath dhekwar <dhekwarkamalnath18@gmail.com>
2024-04-05 15:51:26 +0530

    practice

---
+++ BearandBigBrother.cpp
@@ -0,0 +1,47 @@
+             // ***  ... जय श्री राम ...  ***//
+             
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll            long long int
+#define all(x)        x.begin(),x.end()
+#define rall(x)       x.begin(),x.end()
+#define for(i,a,b)    for(ll i = a ; i<b ; i++)
+#define forr(j,c,d)   for(ll j = c ; j>=d ; j--)
+#define yes           cout<<"Yes"<<endl
+#define no            cout<<"No"<<endl
+#define YES           cout<<"YES"<<endl
+#define NO            cout<<"NO"<<endl
+#define mvi           vector<ll>
+#define mvc           vector<char>
+#define mvs           vector<string>
+#define mpi           map<ll,ll>
+#define mps           map<string,string>
+#define umpi          map<string,string>
+#define umps          map<ll,ll>
+#define nn            cout<<endl
+
+int main()
+{
+   ll a,b;
+   cin>>a>>b;
+  
+   
+   ll cnt = 1;
+
+   a=3*a;
+   b=2*b;
+   while(true){
+    if(a>b) break;
+    cnt++;
+    a=3*a;
+    b=2*b;
+
+   }
+
+
+    cout<<cnt;
+    nn;
+    
+    return 0;
+}
