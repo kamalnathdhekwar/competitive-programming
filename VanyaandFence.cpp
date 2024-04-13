c06669f05063312c98be2f891e7b44081cfdd445
kamalnath dhekwar <dhekwarkamalnath18@gmail.com>
2024-04-13 22:20:15 +0530

    practice

---
+++ VanyaandFence.cpp
@@ -0,0 +1,40 @@
+             // ***  ... जय श्री राम ...  ***//
+             
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll            long long int
+#define all(x)        x.begin(),x.end()
+#define rall(x)       x.begin(),x.end()
+#define for(i,a,b)    for(ll i = a ; i<b ; i++)
+#define forr(j,c,d)   for(ll j = c ; j>=d ; j--)
+#define yes           cout<<"Yes"<<endl
+#define no            cout<<"No"<<endl
+#define YES           cout<<"YES"<<endl
+#define NO            cout<<"NO"<<endl
+#define mvi           vector<ll>
+#define mvc           vector<char>
+#define mvs           vector<string>
+#define mpi           map<ll,ll>
+#define mps           map<string,string>
+#define umpi          map<string,string>
+#define umps          map<ll,ll>
+#define nn            cout<<endl
+
+int main()
+{
+   ll n,h;
+   cin>>n>>h;
+   mvi v(n);
+   ll x=0;
+   for(i,0,n) {
+    cin>>v[i];
+    if(v[i]>h) x++;
+   }
+
+  cout<<x+n<<endl;
+
+
+    
+    return 0;
+}
