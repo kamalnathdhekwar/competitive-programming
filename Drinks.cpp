d437b8fb880bcd7ca90ba0300447888dd3e66b1b
kamalnath dhekwar <dhekwarkamalnath18@gmail.com>
2024-04-23 21:32:00 +0530

    practice

---
+++ Drinks.cpp
@@ -0,0 +1,39 @@
+             // ***  ... जय श्री राम ...  ***//
+             
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll            long long int
+#define all(x)        x.begin(),x.end()
+#define rall(x)       x.begin(),x.end()
+#define for(i,a,b)    for(ll i = a ; i<b ; i++)
+#define forr(j,c,d)   for(ll j = c ; j>=d ; j--)
+#define yes           cout<<"Yes"<<endl
+#define no            cout<<"No"<<endl
+#define YES           cout<<"YES"<<endl
+#define NO            cout<<"NO"<<endl
+#define mvi           vector<ll>
+#define mvc           vector<char>
+#define mvs           vector<string>
+#define mpi           map<ll,ll>
+#define mps           map<string,string>
+#define umpi          map<string,string>
+#define umps          map<ll,ll>
+#define nn            cout<<endl
+
+int main()
+{
+    ll n;
+    cin>>n;
+    mvi v(n);
+
+    double sum = 0;
+    for(i,0,n){
+        cin>>v[i];
+        sum+=v[i];
+    }
+
+    cout<<sum/n<<endl;
+    
+    return 0;
+}
