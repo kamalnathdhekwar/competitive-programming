2fe5f8e0f0e6d8cf77217c0414aef22affd09c9c
kamalnath dhekwar <dhekwarkamalnath18@gmail.com>
2024-05-14 22:09:43 +0530

    practice

---
+++ 1875A.cpp
@@ -0,0 +1,61 @@
+             // ***  ... जय श्री राम ...  ***//
+             
+#include<bits/stdc++.h>
+using namespace std;
+
+// #define ll         long long int
+#define int            int64_t
+#define all(x)        x.begin(),x.end()
+#define rall(x)       x.begin(),x.end()
+#define for(i,a,b)    for(int i = a ; i<b ; i++)
+#define forr(j,c,d)   for(int j = c ; j>=d ; j--)
+#define yes           cout<<"Yes"<<endl
+#define no            cout<<"No"<<endl
+#define YES           cout<<"YES"<<endl
+#define NO            cout<<"NO"<<endl
+#define mvi           vector<int>
+#define mvc           vector<char>
+#define mvs           vector<string>
+#define mpi           map<int,int>
+#define mps           map<string,string>
+#define umpi          map<string,string>
+#define umps          map<int,int>
+#define nn            cout<<"\n"
+
+int32_t main() {
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin>>t;
+    while(t--){
+        int a,b,n;
+        cin>>a>>b>>n;
+
+        mvi v(n);
+
+        for(i,0,n) cin>>v[i];
+
+        sort(all(v));
+
+        int ans = b-1;
+
+        for(i,0,n){
+            b=1;
+            b+=v[i];
+
+            if(b>a) b = a;
+
+            ans+=b-1;
+        }
+
+      //  ans+=b-1;
+
+        cout<<ans+1<<endl;
+    }
+
+
+    
+    return 0;
+}
