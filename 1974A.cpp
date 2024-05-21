45ba844ed84f3093a583e72707996a1b47b80048
kamalnath dhekwar <dhekwarkamalnath18@gmail.com>
2024-05-21 23:13:15 +0530

    practice

---
+++ 1974A.cpp
@@ -0,0 +1,55 @@
+             // ***  ... जय श्री राम ...  ***//
+             
+#include<bits/stdc++.h>
+using namespace std;
+
+// #define ll         long long int
+#define int            int64_t
+#define all(x)        x.begin(),x.end()
+#define rall(x)       x.begin(),x.end()
+#define for(i,a,b)    for(int i = a ; i<b ; i++)
+#define forr(j,c,d)   for(int j = c ; j>=d ; j--)
+#define yes           cout<<"Yes"<<endl
+#define no            cout<<"No"<<endl
+#define YES           cout<<"YES"<<endl
+#define NO            cout<<"NO"<<endl
+#define mvi           vector<int>
+#define mvc           vector<char>
+#define mvs           vector<string>
+#define mpi           map<int,int>
+#define mps           map<string,string>
+#define umpi          map<string,string>
+#define umps          map<int,int>
+#define nn            cout<<"\n"
+
+int solve(int x,int y){
+     if(x==0 && y==0) return 0;
+    if(x<=15 && y==0) return 1;
+    int screen=1;
+    if(y>0)
+        screen+=ceil(y/2.0)-1;
+    int z=15*screen - y*4;
+    if(x>z) screen+=ceil(z/15.0);
+    return screen;
+}
+
+
+int32_t main() {
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin>>t;
+    while(t--){
+        int x,y;
+        cin>>x>>y;
+        
+        cout<<solve(x,y)<<endl;
+
+    }
+
+
+    
+    return 0;
+}
