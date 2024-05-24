cab37956339b2b7fb16f9b4956906a3b9ae87958
kamalnath dhekwar <dhekwarkamalnath18@gmail.com>
2024-05-24 23:40:09 +0530

    practice

---
+++ 1692A.cpp
@@ -0,0 +1,52 @@
+             // ***  ... जय श्री राम ...  ***//
+             
+#include<bits/stdc++.h>
+using namespace std;
+
+// #define ll         long long int
+#define int            int64_t
+#define all(x)        x.begin(),x.end()
+#define rall(x)       x.begin(),x.end()
+#define for(i,a,b)    for(int i = a ; i<b ; i++)
+#define forr(j,c,d)   for(int j = c ; j>=d ; j--)
+#define yes           cout<<"Yes"<<endl
+#define no            cout<<"No"<<endl
+#define YES           cout<<"YES"<<endl
+#define NO            cout<<"NO"<<endl
+#define mvi           vector<int>
+#define mvc           vector<char>
+#define mvs           vector<string>
+#define mpi           map<int,int>
+#define mps           map<string,string>
+#define umpi          map<string,string>
+#define umps          map<int,int>
+#define nn            cout<<"\n"
+
+int32_t main() {
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin>>t;
+    while(t--){
+        mvi v(4);
+        for(i,0,4) cin>>v[i];
+
+        int a = v[0];
+        sort(all(v),greater<int>());
+        int ans  = 0;
+        for(i,0,4) {
+            if(v[i]==a) {
+                ans= i;
+                break;
+            }
+        }
+
+        cout<<ans<<endl;
+    }
+
+
+    
+    return 0;
+}
