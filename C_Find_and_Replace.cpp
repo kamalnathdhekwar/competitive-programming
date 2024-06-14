23e96b5b50c1e8ce0ff9744f98f9938462fbbc94
kamalnath dhekwar <dhekwarkamalnath18@gmail.com>
2024-06-15 00:06:58 +0530

    practice

---
+++ C_Find_and_Replace.cpp
@@ -0,0 +1,54 @@
+             // ***  ... जय श्री राम ...  ***//
+             
+#include<bits/stdc++.h>
+using namespace std;
+
+// #define ll         long long int
+#define int            int64_t
+#define all(x)        x.begin(),x.end()
+#define rall(x)       x.begin(),x.end()
+#define forn(i,a,b)    for(int i = a ; i<b ; i++)
+#define forr(j,c,d)   for(int j = c ; j>=d ; j--)
+#define fora(a,b)     for(auto& a : b)
+#define yes           cout<<"Yes"<<endl
+#define no            cout<<"No"<<endl
+#define YES           cout<<"YES"<<endl
+#define NO            cout<<"NO"<<endl
+#define mvi           vector<int>
+#define mvc           vector<char>
+#define mvs           vector<string>
+#define mpi           map<int,int>
+#define mpc           map<char,int>
+#define umps          unordred_map<int,int>
+#define nn            cout<<"\n"
+
+int32_t main() {
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin>>t;
+    while(t--){
+       int  n;
+       cin>>n;
+       string s;
+       cin>>s;
+       
+       int flag = 0;
+       forn(i,0,n){
+        forn(j,i+1,n){
+            if(s[i]==s[j]) {
+                if((j-i)%2==1) flag = 1;
+            }
+        }
+       }
+
+       if(flag==1) NO;
+       else YES;
+    }
+
+
+    
+    return 0;
+}
