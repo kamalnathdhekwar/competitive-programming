44721e8008199f4480f2726a520fbf5e54559e65
kamalnath dhekwar <dhekwarkamalnath18@gmail.com>
2024-06-16 23:19:39 +0530

    practice

---
+++ A_Petya_and_Strings.cpp
@@ -0,0 +1,40 @@
+             // ***  ... जय श्री राम ...  ***//
+             
+#include<bits/stdc++.h>
+using namespace std;
+
+// #define ll         long long int
+#define int            int64_t
+#define all(x)        x.begin(),x.end()
+#define rall(x)       x.begin(),x.end()
+#define forn(i,a,b)    for(int i = a ; i<b ; i++)
+#define forr(j,c,d)   for(int j = c ; j>=d ; j--)
+#define fora(a,b)     for(auto& a : b)
+#define yes           cout<<"Yes"<<endl
+#define no            cout<<"No"<<endl
+#define YES           cout<<"YES"<<endl
+#define NO            cout<<"NO"<<endl
+#define mvi           vector<int>
+#define mvc           vector<char>
+#define mvs           vector<string>
+#define mpi           map<int,int>
+#define mpc           map<char,int>
+#define umps          unordred_map<int,int>
+#define nn            cout<<"\n"
+
+int32_t main() {
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+     string s,t;
+     cin>>s>>t;
+     forn(i,0,n) if(abs(s[i]-t[i])==32) s[i]=t[i];
+
+     int sum = 0;
+     
+
+
+    
+    return 0;
+}
---
+++ E_Secret_Box.cpp
@@ -0,0 +1,54 @@
+             // ***  ... जय श्री राम ...  ***//
+             
+#include<bits/stdc++.h>
+using namespace std;
+
+// #define ll         long long int
+#define int            int64_t
+#define all(x)        x.begin(),x.end()
+#define rall(x)       x.begin(),x.end()
+#define forn(i,a,b)    for(int i = a ; i<b ; i++)
+#define forr(j,c,d)   for(int j = c ; j>=d ; j--)
+#define fora(a,b)     for(auto& a : b)
+#define yes           cout<<"Yes"<<endl
+#define no            cout<<"No"<<endl
+#define YES           cout<<"YES"<<endl
+#define NO            cout<<"NO"<<endl
+#define mvi           vector<int>
+#define mvc           vector<char>
+#define mvs           vector<string>
+#define mpi           map<int,int>
+#define mpc           map<char,int>
+#define umps          unordred_map<int,int>
+#define nn            cout<<"\n"
+
+int32_t main() {
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin>>t;
+    while(t--){
+        int x,y,z,k,ans=0;
+	cin>>x>>y>>z>>k;
+
+	forn(i,1,x+1){
+		forn(j,1,y+1){
+ 
+			if(k%(i*j)==0 && k/(i*j)<=z){
+				int w = k/(i*j);
+				int positions = (x-i+1) * (y-j+1) * (z-w+1);
+ 
+				ans= max(ans,positions);
+ 
+			}
+		}
+	}
+	cout<<ans<<endl;
+    }
+
+
+    
+    return 0;
+}
