a83def24dc9f29e9e92dbe7f548b09de69445df9
kamalnath dhekwar <dhekwarkamalnath18@gmail.com>
2024-06-22 21:42:42 +0530

    practice

---
+++ A_Wonderful_Permutation.cpp
@@ -0,0 +1,52 @@
+             // ***  ... जय श्री राम ...  ***//
+             
+#include<bits/stdc++.h>
+using namespace std;
+
+// #define ll         long long int
+#define int            int64_t
+#define all(x)        x.begin(),x.end()
+#define rall(x)       x.begin(),x.end()
+#define forn(i,a,b)    for(int i = a ; i<b ; i++)
+#define forr(j,c,d)   for(int j = c ; j>=d ; j--)
+#define fora(a,b)     for(auto& a : b)
+#define yes           cout<<"Yes"<<endl
+#define no            cout<<"No"<<endl
+#define YES           cout<<"YES"<<endl
+#define NO            cout<<"NO"<<endl
+#define mvi           vector<int>
+#define mvc           vector<char>
+#define mvs           vector<string>
+#define mpi           map<int,int>
+#define mpc           map<char,int>
+#define umps          unordred_map<int,int>
+#define nn            cout<<"\n"
+
+int32_t main() {
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin>>t;
+    while(t--){
+        int n,k;
+        cin>>n>>k;
+        mvi v(n);
+
+        forn(i,0,n) cin>>v[i];
+        
+        int c = 0;
+        forn(i,0,k){
+            if(v[i]>k) c++;
+        }
+
+        cout<<c<<endl;
+
+
+    }
+
+
+    
+    return 0;
+}
