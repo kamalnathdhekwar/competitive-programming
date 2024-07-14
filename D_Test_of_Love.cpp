95c6d0c8527176bb5f6f68375d657743c84e08b7
kamalnath dhekwar <dhekwarkamalnath18@gmail.com>
2024-07-14 22:59:12 +0530

    practice

---
+++ D_Test_of_Love.cpp
@@ -0,0 +1,102 @@
+             // ***  ... जय श्री राम ...  ***//
+             
+#include<bits/stdc++.h>
+using namespace std;
+
+// #define ll         long long int
+#define int            int64_t
+#define all(x)        x.begin(),x.end()
+#define rall(x)       x.rbegin(),x.rend()
+#define forn(i,a,b)   for(int i = a ; i<b ; i++)
+#define forr(j,c,d)   for(int j = c ; j>=d ; j--)
+#define fora(a,b)     for(auto& a : b)
+#define yes           cout<<"Yes"<<endl
+#define no            cout<<"No"<<endl
+#define YES           cout<<"YES"<<endl
+#define NO            cout<<"NO"<<endl
+#define pb            push_back
+#define mvi           vector<int>
+#define mvc           vector<char>
+#define mvs           vector<string>
+#define mpi           map<int,int>
+#define mpc           map<char,int>
+#define umps          unordred_map<int,int>
+#define nn            cout<<"\n"
+#define bs            binary_search
+#define mx(x)        *max_element(all(x))
+#define mn(x)        *min_element(all(x))
+
+bool isPrime(int n) { if (n==1) return 0; for (int i=2;i<=round(sqrt(n));++i) if (n%i==0) return 0; return 1; }
+
+
+//..............................................................................................................................................................
+
+
+int32_t main() {
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin>>t;
+    while(t--){
+        int n,m,k;
+        cin>>n>>m>>k;
+        string s;
+        cin>>s;
+ int  wsp = 0,c=m-1;
+ 
+ 
+        int op=0;
+ 
+      for(int ii=0;ii<s.size();ii++    ){
+        
+ 
+        if(s[ii] == 'L'){
+          c=m;
+        }
+ 
+        
+        else if(s[ii] == 'W'){
+          if(c<=0){
+            
+             wsp++;
+          }
+        }
+ 
+ 
+ 
+        else{
+          if(c<=0){
+            
+             op++;
+          
+          }
+        }
+         c--;
+ 
+ 
+ 
+      }
+      
+      if(wsp>k){
+        
+        
+         op++;
+      
+      
+      }
+      
+ 
+      if(op==0   ){
+        cout<<"YES\n";
+ 
+      }
+      else{
+ 
+        cout<<"NO\n";
+      }
+   
+ 
+}
+}
