90816e6d954223ae9114406ce2cb643cdae6b978
kamalnath dhekwar <dhekwarkamalnath18@gmail.com>
2024-07-16 23:32:11 +0530

    practice

--- C_Increasing_Sequence_with_Fixed_OR.cpp
+++ C_Increasing_Sequence_with_Fixed_OR.cpp
@@ -3,48 +3,37 @@
 
 using namespace std;
 
-vector<int> longestBitwiseORSequence(int n) {
-    vector<int> sequence;
-    int current_or = 0;
 
-    // Iterate from n-1 to 1
-    for (int i = n ; i >= 1; i--) {
-        // Check if the OR with current number is still equal to n
-        if ((current_or | i) <= n) {
-            current_or |= i;
-            sequence.push_back(i);
-        }
-    }
-
-    // Sort the sequence to have it in ascending order
-    sort(sequence.begin(), sequence.end());
-    return sequence;
-}
 
 int main() {
     int t;
     cin>>t;
     while(t--){
-        int n;
-   
-    cin >> n;
+        int number;
+        cin >> number;
+        int sequenceLength = 0;
+        vector<int> sequence;
+        vector<int> bitPositions;
+        for (int i = 0; i < 61; i++) {
+            if (number & (1LL << i)) {
+                sequenceLength++;
+                bitPositions.push_back((1LL << i));
+            }
+        }
+        sequenceLength++;
+        sequence.push_back(number);
+        for (auto bitPosition : bitPositions) {
+            if (number - bitPosition > 0) {
+                sequence.push_back(number - bitPosition);
+            }
+        }
 
-    vector<int> sequence = longestBitwiseORSequence(n);
-
-    if(n==1){
-        cout<<n<<endl;
-        cout<<n<<endl;
-    }
-    else {
-        cout<<sequence.size()<<endl;
-        for (int num : sequence) {
-        cout << num << " ";
-    }
-
-    cout << endl;
-    }
-    
-
+        reverse(sequence.begin(), sequence.end());
+        cout << sequence.size() << endl;
+        for (auto element : sequence) {
+            cout << element << " ";
+        }
+        cout << endl;
     }
     return 0;
 }
