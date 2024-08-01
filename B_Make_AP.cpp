ff7e15f0a2a8d478a061216a45efec17a1154ddf
kamalnath dhekwar <dhekwarkamalnath18@gmail.com>
2024-08-01 23:22:37 +0530

    practice

---
+++ B_Make_AP.cpp
@@ -0,0 +1,58 @@
+             // ***  ... जय श्री राम ...  ***//
+             
+#include<bits/stdc++.h>
+using namespace std;
+
+// #define ll         long long int
+#define int            int64_t
+#define all(x)        x.begin(),x.end()
+#define rall(x)       x.rbegin(),x.rend()
+#define forn(i,a,b)   for(int i = a ; i<b ; i++)
+#define forr(j,c,d)   for(int j = c ; j>=d ; j--)
+#define fora(a,b)     for(auto& a : b)
+#define yes           cout<<"Yes"<<endl
+#define no            cout<<"No"<<endl
+#define YES           cout<<"YES"<<endl
+#define NO            cout<<"NO"<<endl
+#define pb            push_back
+#define mvi           vector<int>
+#define mvc           vector<char>
+#define mvs           vector<string>
+#define mpi           map<int,int>
+#define mpc           map<char,int>
+#define umps          unordred_map<int,int>
+#define nn            cout<<"\n"
+#define bs            binary_search
+#define mx(x)        *max_element(all(x))
+#define mn(x)        *min_element(all(x))
+
+bool isPrime(int n) { if (n==1) return 0; for (int i=2;i<=round(sqrt(n));++i) if (n%i==0) return 0; return 1; }
+
+
+//..............................................................................................................................................................
+
+
+int32_t main() {
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin>>t;
+    while(t--){
+      int a,b,c;
+      cin>>a>>b>>c;
+      
+      if((2*b-c)%a==0 && (2*b-c)/a > 0 ) YES;
+      else if((2*b-a)/c > 0 && (2*b-a)%c==0) YES;
+      else if( (c+a) % (2*b)==0) YES;
+      else NO;
+
+      
+
+    }
+
+
+    
+    return 0;
+}
