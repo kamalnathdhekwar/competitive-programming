7d039d2c6b10b03ee258b34f576da5ed9d0c4c69
kamalnath dhekwar <dhekwarkamalnath18@gmail.com>
2024-10-01 22:45:33 +0530

    practice

---
+++ A_Chewbaсca_and_Number.cpp
@@ -0,0 +1,56 @@
+             // ***  ... जय श्री राम ...  ***//
+             
+#include<bits/stdc++.h>
+using namespace std;
+
+
+#define all(x)        x.begin(),x.end()
+#define rall(x)       x.rbegin(),x.rend()
+#define forn(i,a,b)   for(int i = a ; i<b ; i++)
+#define forr(j,c,d)   for(int j = c ; j>=d ; j--)
+#define fora(a,b)     for(auto& a : b)
+#define yes           cout<<"Yes"<<endl
+#define no            cout<<"No"<<endl
+#define YES           cout<<"YES"<<endl
+#define NO            cout<<"NO"<<endl
+#define pb            push_back
+#define mvi           vector<int>
+#define mvc           vector<char>
+#define mvs           vector<string>
+#define mpi           map<int,int>
+#define mpc           map<char,int>
+#define umpi          unordered_map<int,int>
+#define umpc          unordered_map<char,int>
+#define nn            cout<<"\n"
+#define bs            binary_search
+#define mx(x)        *max_element(all(x))
+#define mn(x)        *min_element(all(x))
+
+bool isPrime(int n) { if (n==1) return 0; for (int i=2;i<=round(sqrt(n));++i) if (n%i==0) return 0; return 1; }
+
+int sumv(vector<int>v) { int sum = 0 ;for (int i=0;i<v.size();++i) sum+=v[i]; return sum; }
+
+
+//..............................................................................................................................................................
+
+
+int32_t main() {
+
+     string s;
+     cin>>s;
+
+     int n = s.length();
+
+     forn(i,0,n) {
+       
+        if(s[i]>'4') s[i]='9'-s[i]+'0';
+     }
+
+   if(s[0]=='0') s[0]='9';
+
+     cout<<s<<endl;
+
+
+    
+    return 0;
+}
