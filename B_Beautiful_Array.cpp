c41be505707f1f40dcfee5676c2b52c1934b437f
kamalnath dhekwar <dhekwarkamalnath18@gmail.com>
2024-10-07 22:48:41 +0530

    practice

--- B_Beautiful_Array.cpp
+++ B_Beautiful_Array.cpp
@@ -3,8 +3,8 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-#define ll         long long int
-
+// #define ll         long long int
+#define int            int64_t
 #define all(x)        x.begin(),x.end()
 #define rall(x)       x.rbegin(),x.rend()
 #define forn(i,a,b)   for(int i = a ; i<b ; i++)
@@ -28,29 +28,58 @@ using namespace std;
 #define mn(x)        *min_element(all(x))
 
 bool isPrime(int n) { if (n==1) return 0; for (int i=2;i<=round(sqrt(n));++i) if (n%i==0) return 0; return 1; }
-
 int sumv(vector<int>v) { int sum = 0 ;for (int i=0;i<v.size();++i) sum+=v[i]; return sum; }
 
 
 //..............................................................................................................................................................
 
 
- 
-/* Main()  function */
-int main()
-{
- 
-    int tc=1;
-    cin>>tc;
- 
-    while(tc--)
-    {
-       int k;
-       cin>>k;
+int32_t main() {
 
-       cout<<ceil(sqrt(k))+k<<endl;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin>>t;
+    while(t--){
+      int n,k,b,s;
+      cin>>n>>k>>b>>s;
+
+      if(k*b>s ){
+        cout<<-1<<endl;
+        continue;
+      }
+      else{
+
+        mvi v;
+          
+          if(b*k+k-1<=s)  v.pb(b*k+k-1);
+          else v.pb(s);
+          n--;
+          s-=b*k+k-1;
+
+          while(n--){
+            if(s>0){
+                if(s<k-1)  v.pb(s);
+                else v.pb(k-1);
+                //cout<<k-1<<" ";
+                s-=k-1;
+            }
+            else v.pb(0);
+          }
+
+          if(s>0) cout<<-1<<endl;
+          else {
+            forn(i,0,v.size()) cout<<v[i]<<" ";
+            nn;
+          }
+      }
+
+
 
     }
- 
+
+
+    
     return 0;
 }
