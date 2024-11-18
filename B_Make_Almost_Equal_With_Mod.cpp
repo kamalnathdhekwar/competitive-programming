a40a494fd9fd9c81f4ee74e8996c0a1db31f58b4
kamalnath dhekwar <dhekwarkamalnath18@gmail.com>
2024-11-18 23:51:54 +0530

    practice

--- B_Make_Almost_Equal_With_Mod.cpp
+++ B_Make_Almost_Equal_With_Mod.cpp
@@ -3,7 +3,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-#define ll         long long int
+
 //#define int            int64_t
 #define all(x)        x.begin(),x.end()
 #define rall(x)       x.rbegin(),x.rend()
@@ -26,6 +26,12 @@ using namespace std;
 #define bs            binary_search
 #define mx(x)        *max_element(all(x))
 #define mn(x)        *min_element(all(x))
+using ll        = long long;
+using ld        = long double;
+const ll mod    = 1e9 + 7;
+const ll N      = 2e5 + 10;
+const ll inf    = 1e9;
+const ll linf   = 1e18;
 
 bool isPrime(int n) { if (n==1) return 0; for (int i=2;i<=round(sqrt(n));++i) if (n%i==0) return 0; return 1; }
 int sumv(vector<int>v) { int sum = 0 ;for (int i=0;i<v.size();++i) sum+=v[i]; return sum; }
@@ -42,16 +48,16 @@ int main() {
     int t;
     cin >> t;
     while (t--) {
-        int n;
+        ll n;
         cin >> n;
-        vector<int> v(n);
+        vector<ll> v(n);
         for (auto& i : v) {
             cin >> i;
         }
         ll ans = -1;
         ll x = 2;
         while(x) {
-            set<int> s;
+            set<ll> s;
             for (auto& i : v) {
                 s.insert(i % x);
             }
