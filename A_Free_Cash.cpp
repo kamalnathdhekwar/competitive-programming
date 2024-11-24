54fabe871a64a99d4bed5496a734c47ac57dd936
kamalnath dhekwar <dhekwarkamalnath18@gmail.com>
2024-11-24 23:41:25 +0530

    prcatice

---
+++ A_Free_Cash.cpp
@@ -0,0 +1,67 @@
+             // ***  ... जय श्री राम ...  ***//
+             
+#include<bits/stdc++.h>
+using namespace std;
+
+// #define ll         long long int
+#define int            int64_t
+#define all(x)        x.begin(),x.end()
+#define rall(x)       x.rbegin(),x.rend()
+#define forn(i,a,b)   for(int i = a ; i<b ; i++)
+#define forr(j,c,d)   for(int j = c ; j>=d ; j--)
+#define fora(a,b)     for(auto& a : b)
+#define yes           cout<<"Yes"<<endl
+#define no            cout<<"No"<<endl
+#define YES           cout<<"YES"<<endl
+#define NO            cout<<"NO"<<endl
+#define pb            push_back
+#define mvi           vector<int>
+#define mvc           vector<char>
+#define mvs           vector<string>
+#define mpi           map<int,int>
+#define mpc           map<char,int>
+#define umpi          unordered_map<int,int>
+#define umpc          unordered_map<char,int>
+#define nn            cout<<"\n"
+#define bs            binary_search
+#define mx(x)        *max_element(all(x))
+#define mn(x)        *min_element(all(x))
+
+bool isPrime(int n) { if (n==1) return 0; for (int i=2;i<=round(sqrt(n));++i) if (n%i==0) return 0; return 1; }
+int sumv(vector<int>v) { int sum = 0 ;for (int i=0;i<v.size();++i) sum+=v[i]; return sum; }
+
+
+//..............................................................................................................................................................
+
+
+int32_t main() {
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+   int n;
+	int cash=1;
+	int max=1;
+	cin>>n;
+	int h0,m0;
+	cin>>h0>>m0;
+	int h,m;
+	while(--n){
+	    cin>>h>>m;
+	    if(h0==h && m0==m){
+	        cash++;
+	        if(cash>max){
+	            max=cash;
+	        }
+	    }else{
+	        cash=1;
+	    }
+	    h0=h;
+	    m0=m;
+	}
+	cout<<max;
+	return 0;
+
+    
+    return 0;
+}
